112bb580f3b539460bab408774baf702eeed5da3
Louis Popi <theh2o64@gmail.com>
2016-09-11 10:53:59 +0200

    mdss: (Xiaomi) Force DSI Pipe
    * Force dsi pipe status in certains conditions :
    	- Display OFF = pipe off
    	- Display ON  = pipe on
    	- Dislay Startup = pipe off

--- drivers/video/msm/mdss/mdss_dsi.h
+++ drivers/video/msm/mdss/mdss_dsi.h
@@ -1,4 +1,5 @@
 /* Copyright (c) 2012-2015, The Linux Foundation. All rights reserved.
+ * Copyright (C) 2016 XiaoMi, Inc.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -479,6 +480,7 @@ struct mdss_dsi_ctrl_pdata {
 	bool mmss_clamp;
 	char dlane_swap;	/* data lane swap */
 	bool is_phyreg_enabled;
+	bool dsi_pipe_ready;
 	bool burst_mode_enabled;
 
 	struct dsi_buf tx_buf;
