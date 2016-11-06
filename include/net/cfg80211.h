fe93b1b2c5220d5271af8ceb292e9f95e4712e5f
Srinivas Girigowda <sgirigow@codeaurora.org>
2016-11-06 16:12:45 +0530

    cfg80211: Define macro to indicate support for random mac address for scan
    Define macro to indicate backport support for using random MAC
    addresses for scan while unassociated.
    
    Change-Id: I2b7318ca8f6af29a9eb13d14e8c1e55bd41ae654
    CRs-Fixed: 1082480
    Signed-off-by: Srinivas Dasari <dasaris@codeaurora.org>

--- include/net/cfg80211.h
+++ include/net/cfg80211.h
@@ -70,6 +70,7 @@ struct wiphy;
 #define CFG80211_ABORT_SCAN 1
 #define CFG80211_DISCONNECTED_V2 1
 #define CFG80211_INFORM_BSS_FRAME_DATA 1
+#define CFG80211_SCAN_RANDOM_MAC_ADDR 1
 
 /*
  * wireless hardware capability structures
